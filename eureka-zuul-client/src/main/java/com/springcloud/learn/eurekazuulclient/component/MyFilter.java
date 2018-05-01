d83945d80f8a4eb51b400a48c1746f9b7cab7eb2
OrangeCandies <liuhui.mail@foxmail.com>
2018-05-01 19:40:23 +0800

    add spring cloud config

---
+++ eureka-zuul-client/src/main/java/com/springcloud/learn/eurekazuulclient/component/MyFilter.java
@@ -0,0 +1,45 @@
+package com.springcloud.learn.eurekazuulclient.component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+import javax.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+
+import static org.springframework.cloud.netflix.zuul.filters.support.FilterConstants.PRE_TYPE;
+
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return PRE_TYPE;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        String token = request.getParameter("token");
+        if (token == null) {
+            currentContext.setSendZuulResponse(false);
+            currentContext.setResponseStatusCode(401);
+            try {
+                currentContext.getResponse().getWriter().write("token is empty");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        return null;
+    }
+}
